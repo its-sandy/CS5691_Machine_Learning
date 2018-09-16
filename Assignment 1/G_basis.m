c36e95c7ad2d40f1f787fa2265eb017b1e70ec75
E Santhosh Kumar <santho8edu@gmail.com>
2018-09-17 00:39:12 +0530

    Update G_basis.m

--- Assignment 1/G_basis.m
+++ Assignment 1/G_basis.m
@@ -1,12 +1,12 @@
 ds = dataset('File','Team21-Team30/team30/bivariate_group30/bivariateData/train.txt', 'Delimiter', ' ', 'VarNames', {'x1', 'x2', 'y'}, 'ReadVarNames', false);
 
-M = 5;
+k = 40;
 
 X = double(ds(:,1:end-1));
 Y = double(ds(:,end));
 N = size(X, 1);
 d = size(X, 2);
-phi = get_design_matrix(X, M);
+phi = get_gaussian_design_matrix(X, k);
 
 w = pinv(phi)*Y;
 scatter3(X(:, 1), X(:, 2), Y, 'b'); hold on; scatter3(X(:, 1), X(:, 2), phi*w, 'r')
