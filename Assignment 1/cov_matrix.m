126e2442937600641a1a98eb5c565a7955aa1131
E Santhosh Kumar <santho8edu@gmail.com>
2018-09-17 10:27:07 +0530

    Update cov_matrix.m

--- Assignment 1/cov_matrix.m
+++ Assignment 1/cov_matrix.m
@@ -3,10 +3,7 @@ function C = cov_matrix(inp_mat)
 %(assuming each row is a variable and each column is an observation)
 num_obs = size(inp_mat, 2);
 mean_val = mean(inp_mat, 2);
-C = 0;
 
-for i = 1:num_obs
-    C = C + (inp_mat(:,i)-mean_val)*(inp_mat(:,i)-mean_val)';
-end
+C = ((inp_mat-mean_val)*(inp_mat-mean_val)')./num_obs;
 
 end
