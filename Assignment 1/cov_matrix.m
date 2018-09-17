c79a7e19e22237085b14f2e38a67caff74fd6779
Raghul R <raghulrajasekar@yahoo.co.in>
2018-09-17 21:31:31 +0530

    Modified cov_matrix.m to ensure C is symmetric

--- Assignment 1/cov_matrix.m
+++ Assignment 1/cov_matrix.m
@@ -3,7 +3,7 @@ function C = cov_matrix(inp_mat)
 %(assuming each row is a variable and each column is an observation)
 num_obs = size(inp_mat, 2);
 mean_val = mean(inp_mat, 2);
-
-C = ((inp_mat-mean_val)*(inp_mat-mean_val)')./num_obs;
+inp_mat = inp_mat - mean_val;
+C = (inp_mat*inp_mat')./num_obs;
 
 end
