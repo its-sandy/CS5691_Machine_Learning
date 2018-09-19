ef9a3e163dba11e64922ffc68df158e06095512b
Raghul R <raghulrajasekar@yahoo.co.in>
2018-09-19 20:05:14 +0530

    Changed plots in Task 1

--- Assignment 1/Task1.m
+++ Assignment 1/Task1.m
@@ -1,7 +1,10 @@
 mat = zeros(4096, 150);
 
 ctr = 1;
-for i = [2, 10, 14, 19, 20, 21, 23, 25, 26, 28, 31, 32, 33, 34, 37]
+
+folders = [2, 10, 14, 19, 20, 21, 23, 25, 26, 28, 31, 32, 33, 34, 37];
+
+for i = folders
     for j = 1:10
         mat(:, ctr) = double(reshape(imread(sprintf('EigenAnalysis_Data/Datasets1/64x64/%d/%d.pgm', i, j)), 4096, 1));
         ctr = ctr + 1;
@@ -9,13 +12,15 @@ for i = [2, 10, 14, 19, 20, 21, 23, 25, 26, 28, 31, 32, 33, 34, 37]
 end
 C = cov_matrix(mat);
 [V, D] = eig(C);
-[d,ind] = sort(abs(diag(D)));
+eigenvalues = abs(diag(D));
+[d,ind] = sort(eigenvalues);
 V = V(:,ind);
 
-for img_num = randperm(10, 5)
-    
-    figure('Name', sprintf('Image number: %d', img_num), 'NumberTitle', 'off');
-    orig_img = double(reshape(imread(sprintf('EigenAnalysis_Data/Datasets1/64x64/37/%d.pgm', img_num)), 4096, 1));
+for folder_num = randperm(15, 5)
+    folder = folders(folder_num);
+    img_num = randi(10);
+    figure('Name', sprintf('Folder number: %d\nImage number: %d', folder, img_num), 'NumberTitle', 'off');
+    orig_img = double(reshape(imread(sprintf('EigenAnalysis_Data/Datasets1/64x64/%d/%d.pgm', folder, img_num)), 4096, 1));
     ctr = 1;
     for i = [1, 10, 20, 40, 80, 160, 320, 640]
         recon_img = zeros(4096, 1);
@@ -23,7 +28,10 @@ for img_num = randperm(10, 5)
             recon_img = recon_img + (orig_img'*V(:, end-j+1))*V(:, end-j+1);
         end
         recon_img = uint8(reshape(recon_img, 64, 64));
-        subplot(2, 4, ctr), imshow(recon_img);
+        subplot(3, 4, ctr), imshow(recon_img);
+        title(sprintf("Eigenvectors used:\n%d", i));
         ctr = ctr + 1;
     end
+    subplot('Position', [0.4 0.1 0.2 0.2]), imshow(uint8(reshape(orig_img, 64, 64)));
+    title("Original Image");
 end
