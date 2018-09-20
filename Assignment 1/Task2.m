c554ab6977a91c9f169e51270d7fd6689b8db711
raja <you@example.com>
2018-09-20 17:08:14 +0530

    raja task params

--- Assignment 1/Task2.m
+++ Assignment 1/Task2.m
@@ -1,4 +1,4 @@
-rng(0);
+rng(5);
 bN=25;
 bM=1;
 bL=1e-2;
@@ -7,7 +7,7 @@ Ermstest=zeros(3,4);
 Ermstrain=zeros(3,4);
 for N = [20,40,100]
     X = sort(rand(N, 1));
-    X2 = sort(rand(N/5, 1));
+    X2 = sort(rand(N/4, 1));%test data is 20% of total data
     Y = awgn(exp(sin(2*pi*X)), 10);
     Y2 = awgn(exp(sin(2*pi*X2)), 10);
     ctr = 1;
@@ -19,7 +19,7 @@ for N = [20,40,100]
         for M=[1,10,20,30]
             i2=i2+1;
             phi = zeros(N, M+1);
-            phi2 = zeros(N/5, M+1);
+            phi2 = zeros(N/4, M+1);
             a = zeros(M+1, M+1);
 
             for i=1:M+1
@@ -32,13 +32,15 @@ for N = [20,40,100]
             c = phi'*Y;
             w = linsolve(a+lambda*eye(M+1), c);
             %means w= pinv(a+lambda*eye(M+1))*c;
+            
             subplot(3, 4, ctr);
             plot(X, Y, 'b.'); hold on
             plot(X2, Y2, 'm.'); hold on
             plot(0:0.01:1, polyval(flipud(w), 0:0.01:1), 'r-'); hold on
             plot(0:0.01:1, exp(sin(2*pi*(0:0.01:1))), 'g-');
-            ylim([-1,3.5]);
+            ylim([-1,3.5]);            
             title(sprintf("N: %d M: %d\nlambda: %0.1e",N, M,lambda));
+            
             if N==100
                 newerrtrn=(lambda*w'*w+sum((phi*w)-Y).^2);
                 newerrtst=(lambda*w'*w+sum((phi2*w)-Y2).^2);  
@@ -53,20 +55,21 @@ for N = [20,40,100]
                     bL=lambda;
                     btrainX=X;
                     btrainY=Y;
+                    bphi=phi;
+                    bphi2=phi2;
                     btestX=X2;
                     btestY=Y2;
                     bw=w;
                 end
             end
+            
             ctr=ctr + 1;
         end
     end
 end
-figure('Name', sprintf('Polynomial Curve Fitting %d',N), 'NumberTitle', 'off');
-plot(btrainX, btrainY, 'b.'); hold on
-plot(btestX, btestY, 'm.'); hold on
-plot(0:0.01:1, polyval(flipud(bw), 0:0.01:1), 'r-'); hold on
-plot(0:0.01:1, exp(sin(2*pi*(0:0.01:1))), 'g-');
+figure('Name', sprintf('Polynomial Curve Fitting %d %d %d',N,bM,bL), 'NumberTitle', 'off');
+plot(btrainY, bphi*bw, 'bo'); hold on
+plot(btestY, bphi2*bw, 'ro'); hold on
 Ermstest
 Ermstrain
 
