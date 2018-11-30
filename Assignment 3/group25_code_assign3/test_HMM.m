3617623c0de07c4bb1538cc5e11914a21645c11d
Raghul R <raghulrajasekar@yahoo.co.in>
2018-11-30 20:42:27 +0530

    Made HMM work on PC

--- Assignment 3/group25_code_assign3/test_HMM.m
+++ Assignment 3/group25_code_assign3/test_HMM.m
@@ -1,7 +1,11 @@
 function alpha_out = test_HMM(num_examples, test_file, model_file)
 %assumes each row corresponds to a data point and each column corresponds
 %to an attribute size(x) = N*d
-system(sprintf("%s %s %s",fullfile('hmm-1.04','hmm-1.04','./test_hmm'), test_file, model_file));
+if ispc
+    system(sprintf('bash -c \"hmm-1.04/hmm-1.04/test_hmm %s %s\"', test_file, model_file));
+else
+    system(sprintf("%s %s %s",fullfile('hmm-1.04','hmm-1.04','./test_hmm'), test_file, model_file));
+end
 in_file = fopen(fullfile('alphaout'), 'r');
 [alpha_out, ~] = fscanf(in_file, '%f', num_examples);
 fclose(in_file);
