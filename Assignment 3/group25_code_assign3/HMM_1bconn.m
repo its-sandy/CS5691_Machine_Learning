aa1dcaca7e5e1d315fceb26b6cb18767004f567a
Raghul R <raghulrajasekar@yahoo.co.in>
2018-12-01 17:12:51 +0530

    Basic code for Dataset 1(b) (connected) added in HMM_1bconn.m

--- Assignment 3/group25_code_assign3/HMM_1bconn.m
+++ Assignment 3/group25_code_assign3/HMM_1bconn.m
@@ -1,10 +1,10 @@
 data_conn = zeros(0,39);
-
-in_file = fopen(fullfile('..','group25_data_assign3','connected','test1','25','49oa.mfcc'), 'r');
+file = '94a.mfcc';
+in_file = fopen(fullfile('..','group25_data_assign3','connected','test1','25',file), 'r');
 if (in_file == -1) 
     error('oops, file can''t be read'); 
 end 
-fprintf("opened file %s\n", fullfile('..','group25_data_assign3','connected','test1','25','49oa.mfcc'));
+fprintf("opened file %s\n", fullfile('..','group25_data_assign3','connected','test1','25',file));
 [d, count] = fscanf(in_file, '%d', 1);
 if (count == -1)
     fprintf("empty file\n");
@@ -19,15 +19,72 @@ fclose(in_file);
 
 conn_sequence = [(k_means_test(data_conn(:,1:38),centers)-1) data_conn(:,39)];
 
+best = -inf;
+i1best = -1;
+i2best = -1;
+strbest = '';
+mapch = ['4', '9', 'o'];
+
+tot = 0;
 out_file = fopen(fullfile('hmm-1.04','hmm-1.04','GeneratedSequences_1b','conn_sequence.seq'), 'w');
-for i = 1:size(conn_sequence,1)
-    for j = 1:i
-        fprintf(out_file,"%d ",conn_sequence(j, 1));   
+for i1 = 1:size(conn_sequence,1)-2
+    for i2 = i1+1:size(conn_sequence,1)-1
+        for j = 1:size(conn_sequence,1)
+            fprintf(out_file,"%d ",conn_sequence(j, 1));
+            if j==i1 || j==i2 || j==size(conn_sequence,1)
+                fprintf(out_file,"\n");
+                tot = tot+1;
+            end
+        end
     end
-    fprintf(out_file,"\n");
 end
 fclose(out_file);
 
-alpha_4 = test_HMM(size(conn_sequence, 1), 'hmm-1.04/hmm-1.04/GeneratedSequences_1b/conn_sequence.seq', 'hmm-1.04/hmm-1.04/GeneratedSequences_1b/train_sequence_4.seq.hmm');
-alpha_9 = test_HMM(size(conn_sequence, 1), 'hmm-1.04/hmm-1.04/GeneratedSequences_1b/conn_sequence.seq', 'hmm-1.04/hmm-1.04/GeneratedSequences_1b/train_sequence_9.seq.hmm');
-alpha_o = test_HMM(size(conn_sequence, 1), 'hmm-1.04/hmm-1.04/GeneratedSequences_1b/conn_sequence.seq', 'hmm-1.04/hmm-1.04/GeneratedSequences_1b/train_sequence_o.seq.hmm');
+alpha_4 = test_HMM(tot, 'hmm-1.04/hmm-1.04/GeneratedSequences_1b/conn_sequence.seq', 'hmm-1.04/hmm-1.04/GeneratedSequences_1b/train_sequence_4.seq.hmm');
+alpha_9 = test_HMM(tot, 'hmm-1.04/hmm-1.04/GeneratedSequences_1b/conn_sequence.seq', 'hmm-1.04/hmm-1.04/GeneratedSequences_1b/train_sequence_9.seq.hmm');
+alpha_o = test_HMM(tot, 'hmm-1.04/hmm-1.04/GeneratedSequences_1b/conn_sequence.seq', 'hmm-1.04/hmm-1.04/GeneratedSequences_1b/train_sequence_o.seq.hmm');
+
+ctr = 1;
+for i1 = 1:size(conn_sequence,1)-2
+    for i2 = i1+1:size(conn_sequence,1)-1
+        [val, ind] = max([alpha_4(ctr:ctr+2), alpha_9(ctr:ctr+2), alpha_o(ctr:ctr+2)], [], 2);
+        if sum(val)>best
+            i1best = i1;
+            i2best = i2;
+            strbest = mapch(ind);
+            best = sum(val);
+        end
+        ctr = ctr+3;
+    end
+end
+
+tot = 0;
+out_file = fopen(fullfile('hmm-1.04','hmm-1.04','GeneratedSequences_1b','conn_sequence.seq'), 'w');
+for i1 = 1:size(conn_sequence,1)-1
+    for j = 1:size(conn_sequence,1)
+        fprintf(out_file,"%d ",conn_sequence(j, 1));
+        if j==i1 || j==size(conn_sequence,1)
+            fprintf(out_file,"\n");
+            tot = tot+1;
+        end
+    end
+end
+fclose(out_file);
+
+alpha_4 = test_HMM(tot, 'hmm-1.04/hmm-1.04/GeneratedSequences_1b/conn_sequence.seq', 'hmm-1.04/hmm-1.04/GeneratedSequences_1b/train_sequence_4.seq.hmm');
+alpha_9 = test_HMM(tot, 'hmm-1.04/hmm-1.04/GeneratedSequences_1b/conn_sequence.seq', 'hmm-1.04/hmm-1.04/GeneratedSequences_1b/train_sequence_9.seq.hmm');
+alpha_o = test_HMM(tot, 'hmm-1.04/hmm-1.04/GeneratedSequences_1b/conn_sequence.seq', 'hmm-1.04/hmm-1.04/GeneratedSequences_1b/train_sequence_o.seq.hmm');
+
+ctr = 1;
+for i1 = 1:size(conn_sequence,1)-1
+    [val, ind] = max([alpha_4(ctr:ctr+1), alpha_9(ctr:ctr+1), alpha_o(ctr:ctr+1)], [], 2);
+    if sum(val)>best
+        i1best = i1;
+        i2best = -1;
+        strbest = mapch(ind);
+        best = sum(val);
+    end
+    ctr = ctr+2;
+end
+
+fprintf('Best string is %d, %d: %s (%f)\n', i1best, i2best, strbest, best);
